8f120ec2ede115c28851c6eecb6163fc372aaf55
sanglt <sanglt@imt-soft.com>
2023-03-25 23:52:38 +0700

    - update UI template path

--- src/component/admin/AdminSidebar.js
+++ src/component/admin/AdminSidebar.js
@@ -8,7 +8,7 @@ export default function AdminSidebar() {
 
                 <div className="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div className="image">
-                        <img src="../../admin-lte/dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
+                        <img src="../../../admin-lte/dist/img/user2-160x160.jpg" className="img-circle elevation-2" alt="User Image" />
                     </div>
                     <div className="info">
                         <a href="#" className="d-block">Alexander Pierce</a>
