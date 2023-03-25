eb3181d6f292f3d5768c90dda9eb0a7a3406ffba
sanglt <sanglt@imt-soft.com>
2023-03-25 16:50:06 +0700

    - integrate delete user api

--- src/component/admin/admin-modules/admission-user/IndexUser.js
+++ src/component/admin/admin-modules/admission-user/IndexUser.js
@@ -2,11 +2,26 @@ import React, { useState, useEffect } from "react";
 import API, { endpoints } from "../../../../API";
 import { useNavigate, useParams } from 'react-router-dom';
 import { Outlet, Link } from "react-router-dom";
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 export default function IndexUser() {
     const nav = useNavigate()
     const [users, setUsers] = useState([])
-    const goDetail = (id) => nav(`/admin/user/detail/${id}`)
+    const [show, setShow] = useState(false);
+    const [refreshKey, setRefreshKey] = useState(0); //to refresh user list after delete the user
+
+    const [modalShow, setModalShow] = useState(false);
+    const [deleteUserId, setDeleteUserId] = useState(null);
+
+    const handleModalClose = () => setModalShow(false);
+    const handleModalShow = (userId) => {
+        return () => {
+            setDeleteUserId(userId);
+            setModalShow(true);
+        }
+    }
 
     useEffect(() => {
         const loadUsers = async () => {
@@ -15,16 +30,52 @@ export default function IndexUser() {
             })
         }
         loadUsers()
-    }, [])
+    }, [refreshKey])
 
+    const deleteUser = () => {
+        handleModalClose();
+        API.delete(endpoints["user"] + `${deleteUserId}/`).then(res => {
+            console.log(res)
+            if (res && res.status == 204) {
+                setShow(true)
+                // loadUsers2()
+                setTimeout(() => {
+                    setShow(false)
+                    setRefreshKey(oldKey => oldKey + 1)
+                }, 2000);
+            }
+        })
+    }
 
     return (
         <div className="content-wrapper">
+
+            <Modal show={modalShow} onHide={handleModalClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Xác nhận</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Bạn có muốn xóa người dùng?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleModalClose}>
+                        Đóng
+                    </Button>
+                    <Button variant="primary" onClick={deleteUser}>
+                        Xóa
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
             <h1>Danh sách tài khoản</h1>
+            {show &&
+                <Alert key={'success'} variant={'success'} onClose={() => setShow(false)} dismissible>
+                    Đã xóa người dùng
+                </Alert>
+            }
+
             <div className="card">
                 <div className="card-header">
-                    <a href="/admin/user/create" type="button" className="btn btn-primary btn-block" style={{ width: '200px', float: 'right' }}>
-                        <i className="fas fa-utensil-spoon"></i> Thêm mới
+                    <a href="/admin/user/create" type="button" className="btn btn-primary btn-block" style={{ width: '150px', float: 'right' }}>
+                        <i className="fa fa-plus"></i> Thêm mới
                     </a>
 
                 </div>
@@ -45,22 +96,18 @@ export default function IndexUser() {
                                 <tr key={u.id}>
                                     <td>{index + 1}</td>
                                     <td>
-                                    {/* {u.last_name + ' ' + u.first_name} */}
-                                    <Link className="btn-item auction-btn mr-2" to={`detail/${u.id}`}>
-                                    {u.last_name + ' ' + u.first_name}
-                                    </Link>
-                                        {/* <a className="">
-                                         {u.last_name + ' ' + u.first_name}
-                                        </a> */}
+                                        <Link className="btn-item auction-btn mr-2" to={`detail/${u.id}`}>
+                                            {u.last_name + ' ' + u.first_name}
+                                        </Link>
                                     </td>
                                     <td>{u.phone}</td>
-                                    <td>{u.is_superuser == true ? 'Ban tư vấn' : 'Sinh viên'}</td>
                                     <td>{u.email}</td>
+                                    <td>{u.is_superuser == true ? 'Ban tư vấn' : 'Sinh viên'}</td>
                                     <td>
                                         <button type="button" className="btn btn-default mr-2">
                                             <i className="fas fa-pencil-alt"></i>
                                         </button>
-                                        <button type="button" className="btn btn-danger">
+                                        <button type="button" className="btn btn-danger" onClick={handleModalShow(u.id)}>
                                             <i className="far fa-trash-alt"></i>
                                         </button>
                                     </td>
